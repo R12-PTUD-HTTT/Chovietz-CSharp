0513c8d9b8ae24b8f1a10ebca5f496ee0ad58f99
ngocducvo <55821615+ngocducvo@users.noreply.github.com>
2022-01-13 00:41:27 +0700

    construct shipping application form
    build model
    add api get all form

---
+++ dichothuez/Controllers/ShipAppFormController.cs
@@ -0,0 +1,35 @@
+﻿using dichothuez.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dichothuez.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShipAppFormController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public ShipAppFormController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("dichothuezConnection"));
+            var shipAppForm = dbClient.GetDatabase("dichothuez").GetCollection<Shipping_Application_Form>("Shipping_Application_Form").AsQueryable();
+            return new JsonResult(shipAppForm);
+        }
+    }
+}
